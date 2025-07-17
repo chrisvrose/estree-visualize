d44ce3bcdb9bf8878b54595c4d5f0d6bc9f98c7c
Atreya Bain <atreyabain@gmail.com>
2025-07-17 12:59:05 +0100

    [add] menu icons

--- src/components/OutputTab.svelte
+++ src/components/OutputTab.svelte
@@ -1,46 +1,67 @@
 <script lang="ts">
-	import type { Program } from "estree";
-	import ErrorBox from "./ErrorBox.svelte";
-    import {Tabs} from '@skeletonlabs/skeleton-svelte'
-    import CodeMirror from "svelte-codemirror-editor";
-	import { isAnyError } from "$lib/parse";
-	import AstDisplay from "./ASTDisplay.svelte";
-	import { SvelteFlowProvider } from "@xyflow/svelte";
-	import { oneDark } from "@codemirror/theme-one-dark";
-    import {json} from '@codemirror/lang-json'
-    
-    type CurrentTab = 'error'|'ast'|'astString' | string;
-    
-    type OutputTabProps = {
-        sourceAST: Program | null,
-        sourceASTString: string | null,
-        error: string | null
-    }
+	import type { Program } from 'estree';
+	import ErrorBox from './ErrorBox.svelte';
+	import { Tabs } from '@skeletonlabs/skeleton-svelte';
+	import CodeMirror from 'svelte-codemirror-editor';
+	import { isAnyError } from '$lib/parse';
+	import AstDisplay from './ASTDisplay.svelte';
+	import { SvelteFlowProvider } from '@xyflow/svelte';
+	import { oneDark } from '@codemirror/theme-one-dark';
+	import { json } from '@codemirror/lang-json';
+	import IconNetwork from '@lucide/svelte/icons/network';
+	import IconJSON from '@lucide/svelte/icons/file-json-2';
+	import IconNoErrorsAlert from '@lucide/svelte/icons/spell-check';
+	import IconErrorsAlert from '@lucide/svelte/icons/spell-check-2';
 
-    const {sourceAST,sourceASTString,error}:OutputTabProps = $props();
-    
-    let selectedTab:CurrentTab = $state('ast');
+	type CurrentTab = 'error' | 'ast' | 'astString' | string;
+
+	type OutputTabProps = {
+		sourceAST: Program | null;
+		sourceASTString: string | null;
+		error: string | null;
+	};
+
+	const { sourceAST, sourceASTString, error }: OutputTabProps = $props();
+
+	let selectedTab: CurrentTab = $state('ast');
 </script>
 
-<Tabs value={selectedTab} onValueChange={e=>selectedTab = e.value}>
-    {#snippet list()}
-        <Tabs.Control value="ast">Graphical AST</Tabs.Control>
-        <Tabs.Control value="astString">JSON AST</Tabs.Control>
-        <Tabs.Control value="error">Errors {#if isAnyError(error)}
-                <span class="badge-icon preset-filled-error-500 absolute -right-0 -top-0 z-10">!</span>
-        {/if}</Tabs.Control>
-    {/snippet}
-    {#snippet content()}
-        <Tabs.Panel value="astString">
-            <CodeMirror value={sourceASTString} basic={true} readonly  theme={oneDark} lang={json()}></CodeMirror>
-        </Tabs.Panel>
-        <Tabs.Panel value="ast">
-        <SvelteFlowProvider>
-            <AstDisplay ast={sourceAST} />
-        </SvelteFlowProvider>
-        </Tabs.Panel>
-        <Tabs.Panel value="error">
-            <ErrorBox error={error}/>
-        </Tabs.Panel>
-    {/snippet}
+<Tabs value={selectedTab} onValueChange={(e) => (selectedTab = e.value)}>
+	{#snippet list()}
+		<Tabs.Control value="ast">
+			{#snippet lead()}<IconNetwork size={20} />{/snippet}
+			Tree</Tabs.Control
+		>
+		<Tabs.Control value="astString">
+			{#snippet lead()}<IconJSON size={20} />{/snippet}
+			JSON</Tabs.Control
+		>
+		<Tabs.Control value="error"
+			>
+			{#snippet lead()}
+                {#if isAnyError(error)}
+                <IconErrorsAlert size={20} class="text-error-500"/>
+                {:else}
+                <IconNoErrorsAlert size={20} />
+                {/if}
+            {/snippet}
+                Errors {#if isAnyError(error)}
+				<span class="badge-icon preset-filled-error-500 absolute -top-0 -right-0 z-10">!</span>
+			{/if}</Tabs.Control
+		>
+	{/snippet}
+	{#snippet content()}
+		<Tabs.Panel value="astString">
+			<CodeMirror value={sourceASTString} basic={true} readonly theme={oneDark} lang={json()}
+			></CodeMirror>
+		</Tabs.Panel>
+		<Tabs.Panel value="ast">
+			<SvelteFlowProvider>
+				<AstDisplay ast={sourceAST} />
+			</SvelteFlowProvider>
+		</Tabs.Panel>
+		<Tabs.Panel value="error">
+			<ErrorBox {error} />
+		</Tabs.Panel>
+	{/snippet}
 </Tabs>
